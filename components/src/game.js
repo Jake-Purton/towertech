eee81959df8f79d389468fae15384d6162b847a3
Chris Harris <christian.harris-3@student.manchester.ac.uk>
2025-01-30 16:07:25 +0000

    created base phaser code structure for the project
    added a player with arrow key movement control

---
+++ components/src/game.js
@@ -0,0 +1,70 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import * as Phaser from 'phaser';
+import Player from './player.js'
+
+export default class Game extends Phaser.Scene{
+    constructor(){
+        super('GameScene');
+    }
+    preload() {
+        this.load.image('body','/game_images/body_image.png');
+        this.load.image('leg','/game_images/leg.png');
+        this.load.image('arm','/game_images/arm.png');
+    }
+    create() {
+        this.players = new Map([]);
+        this.players.set('TempPlayerID', new Player(this, 100, 100));
+
+        this.kprs = this.input.keyboard.createCursorKeys();
+
+    }
+    update(time, delta) {
+        this.dummy_input();
+        for (let [key, player] of this.players) {
+            player.game_tick(delta);
+        }
+    }
+    take_input(input){
+        console.log(input, input.has(input.get('PlayerID')), input.get('PlayerID'));
+        if (this.players.has(input.get('PlayerID'))){
+            let player = this.players.get(input.get('PlayerID'));
+            player.input_key(input.get('Key'), input.get('Direction'));
+        }
+    }
+    dummy_input(){
+        // dummy method that attempts to recreate how inputs would be taken
+        if (this.kprs.up.isDown){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','UP'],['Direction','Down']]))
+        }
+        if (this.kprs.down.isDown){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','DOWN'],['Direction','Down']]))
+        }
+        if (this.kprs.right.isDown){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','RIGHT'],['Direction','Down']]))
+        }
+        if (this.kprs.left.isDown){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','LEFT'],['Direction','Down']]))
+        }
+
+        if (this.kprs.up.isUp){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','UP'],['Direction','Up']]))
+        }
+        if (this.kprs.down.isUp){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','DOWN'],['Direction','Up']]))
+        }
+        if (this.kprs.right.isUp){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','RIGHT'],['Direction','Up']]))
+        }
+        if (this.kprs.left.isUp){
+            this.take_input(new Map([['PlayerID', 'TempPlayerID'],
+                ['Key','LEFT'],['Direction','Up']]))
+        }
+    }
+}
---
+++ components/src/player.js
@@ -0,0 +1,68 @@
+import * as Phaser from 'phaser';
+const Vec = Phaser.Math.Vector2;
+
+export default class Player extends Phaser.GameObjects.Container{
+    constructor(scene, x, y){
+
+        // create body parts
+        let body = new Phaser.Physics.Arcade.Sprite(scene, 0, 0, 'body');
+        let left_leg = new Phaser.Physics.Arcade.Sprite(scene, 10, 12, 'leg');
+        let right_leg = new Phaser.Physics.Arcade.Sprite(scene, -10, 12, 'leg');
+        let left_arm = new Phaser.Physics.Arcade.Sprite(scene, -14, -3, 'arm');
+        let right_arm = new Phaser.Physics.Arcade.Sprite(scene, 14, -3, 'arm');
+        left_leg.setScale(-1,1);
+        right_arm.setScale(-1,1);
+
+        // create phaser stuff
+        super(scene, x, y, [body, left_leg, right_leg, left_arm, right_arm]);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        // this.setCollideWorldBounds(true);
+
+        // assign body parts
+        this.body_object = body;
+        this.left_leg = left_leg;
+        this.right_leg = right_leg;
+        this.left_arm = left_leg;
+        this.right_arm = right_leg;
+
+        // variables
+        this.velocity = new Vec(0,0);
+        this.key_inputs = new Map([
+            ['UP', 0],
+            ['DOWN', 0],
+            ['LEFT', 0],
+            ['RIGHT', 0]])
+        this.move_direction = new Vec(0,0);
+
+        // constants
+        this.speed = 0.02;
+        this.drag = 0.9;
+
+    }
+    game_tick(delta_time){ //function run by game.js every game tick
+
+        this.move_direction = new Vec(this.key_inputs.get('RIGHT')-this.key_inputs.get('LEFT'),
+                                      this.key_inputs.get('DOWN')-this.key_inputs.get('UP'))
+
+        this.move_direction.normalize();
+        this.move_direction.scale(this.speed * delta_time);
+
+        this.velocity.add(this.move_direction);
+        this.velocity.x *= this.drag;
+        this.velocity.y *= this.drag;
+
+        this.body.position.add(this.velocity);
+    }
+    input_key(key, direction){
+        if (direction === 'Down'){
+            this.key_inputs.set(key, 1);
+        } else {
+            this.key_inputs.set(key, 0);
+        }
+    }
+    check_collision(players){
+
+    }
+}
