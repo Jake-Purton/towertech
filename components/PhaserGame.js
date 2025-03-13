e07dc8181e6890a75461714ceb70a9d54f9e1d7b
Jake-Purton <jakecapurton@gmail.com>
2025-03-13 17:49:54 +0000

    fixed something that i broke

--- components/PhaserGame.js
+++ components/PhaserGame.js
@@ -9,14 +9,14 @@ const PhaserGame = () => {
 
   useEffect(() => {
 
-    socket.on('updateUsers', (userList) => {
-      if (userList.length === 0) {
-        // COMMENT THE BELOW LINE OUT IF YOU DONT WANT TO GET KICKED OUT OFF THE PAGE
-        router.push('/join/room')
-      }
-    });
+    // socket.on('updateUsers', (userList) => {
+    //   if (userList.length === 0) {
+    //     // COMMENT THE BELOW LINE OUT IF YOU DONT WANT TO GET KICKED OUT OFF THE PAGE
+    //     router.push('/join/room')
+    //   }
+    // });
 
-    socket.emit('getUsers');
+    // socket.emit('getUsers');
 
     if (typeof window !== 'undefined') {
       import('phaser').then(Phaser => {
